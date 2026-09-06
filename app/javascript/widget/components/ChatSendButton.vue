847b6244606836b98a037ce42a5d201cf62eb213
Sang Truong <45520864+sangtrx@users.noreply.github.com>
2026-09-07 04:39:57 +0700

    fix(widget): mirror send icon in RTL

--- app/javascript/widget/components/ChatSendButton.vue
+++ app/javascript/widget/components/ChatSendButton.vue
@@ -30,7 +30,12 @@ export default {
     :disabled="disabled"
     class="min-h-8 min-w-8 flex items-center justify-center ml-1"
   >
-    <FluentIcon v-if="!loading" icon="send" :style="`color: ${color}`" />
+    <FluentIcon
+      v-if="!loading"
+      icon="send"
+      class="rtl:rotate-180"
+      :style="`color: ${color}`"
+    />
     <Spinner v-else size="small" />
   </button>
 </template>
